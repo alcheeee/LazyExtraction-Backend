a1f8250846907fe6ba0444ff2c8fc88b9f774673
alcheee <romain_alex@outlook.com>
2024-04-11 18:22:03 -0400

    CHANGE LOG 0.1104:
    - MySQL Database setup for scaling with migrations
    
    TODO:
    
    - Integrate all item stuff with user interactions/corporations

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Settings:
 
     # Project
     PROJECT_NAME: str = "RPGapi | Development"
-    VERSION: str = "0.1101"
+    VERSION: str = "0.1104"
 
     # Security
     SECRET_KEY: str = secrets.token_urlsafe(32)
