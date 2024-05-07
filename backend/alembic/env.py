e4896819997a0211eaa02b4dbf0348e04f31e52b
alcheee <romain_alex@outlook.com>
2024-05-07 19:36:16 -0400

    CHANGE LOG 0.1231:
    - Added Alembic database migrations to the docker container
    
    TODO:
    
    List for after I migrate from SQLModel queries to pure SQLAlchemy:
    
    - Posting on black market
    
    - CORPORATIONS
     - Private message invites
     - Activity Logic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,46 +1,33 @@
 import asyncio
 import os
-from logging.config import fileConfig
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+import sqlmodel
+from app.models.corp_models import *
+from app.models.models import *
+from app.models.item_models import *
+from app.models.other_models import *
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
+config = context.config
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-from sqlmodel import SQLModel
+target_metadata = sqlmodel.SQLModel.metadata
 
-target_metadata = SQLModel.metadata
-
-config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -61,10 +48,6 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
 
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
@@ -79,8 +62,6 @@ async def run_async_migrations() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
     asyncio.run(run_async_migrations())
 
 
---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Waiting for PostgreSQL to be ready..."
+while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
+  sleep 0.1
+done
+
+echo "Running Alembic Migrations"
+alembic upgrade head
+
+echo "Starting FastAPI application"
+exec uvicorn app.main:app --host ${FASTAPI_HOST} --port ${FASTAPI_PORT} --workers 4 --reload
